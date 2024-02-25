44dcc36910a4707f9822353edf6c7a77313c70a3
Alexander Thomas Adams <icokid@gmail.com>
2024-02-25 12:26:38 -0600

    created resource embedder

---
+++ tools/embedder.go
@@ -0,0 +1,25 @@
+package tools
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func EmbedResource(fileName, resourceName string) (n int) {
+	newResource, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resourceFile, err := os.OpenFile("../resources/resources.go", os.O_CREATE|os.O_RDWR|os.O_APPEND, os.ModeAppend)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	n, err = resourceFile.WriteString(fmt.Sprintf("var %v = %#v", resourceName, newResource))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return
+}
